a52de407465b9386ff2f230ae0b5c29ecf6587f0
Daniela Guardia <138531935+DaniJGP@users.noreply.github.com>
2025-08-05 12:25:33 -0400

    feat: add fade-in/slide animations to all sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,17 @@
 const sections = document.querySelectorAll('.section');
 const navLinks = document.querySelectorAll('#navbar .navbar-item');
+const animatedElements = document.querySelectorAll('.animate');
 
 const observerOptions = {
     root: null,
     rootMargin: '0px',
     threshold: 0.55,
+};
+
+const animatorOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.25,
 };
 
 const observerCallback = (entries, observer) => {
@@ -21,8 +28,22 @@ const observerCallback = (entries, observer) => {
     });
 };
 
+const animatorCallback = (entries, observer) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('is-visible');
+          observer.unobserve(entry.target);
+        };
+    });
+};
+
 const observer = new IntersectionObserver(observerCallback, observerOptions);
+const animator = new IntersectionObserver(animatorCallback, animatorOptions);
 
 sections.forEach((section) => {
     observer.observe(section);
 });
+
+animatedElements.forEach((element) => {
+  animator.observe(element);
+})
